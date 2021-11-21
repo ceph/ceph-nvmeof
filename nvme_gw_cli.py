4411e071ff6931a725603417d03a0d7acc3fdf8b
Mykola Golub <mykola.golub@clyso.com>
2021-11-21 08:27:21 +0000

    nvme_gw_cli: fix cli.cmd decorator when args are empty
    Signed-off-by: Mykola Golub <mykola.golub@clyso.com>

--- nvme_gw_cli.py
+++ nvme_gw_cli.py
@@ -54,7 +54,7 @@ class Parser:
             # attribute to point to the subcommand's associated function
             for arg in args:
                 parser.add_argument(*arg[0], **arg[1])
-                parser.set_defaults(func=func)
+            parser.set_defaults(func=func)
             return func
 
         return decorator
