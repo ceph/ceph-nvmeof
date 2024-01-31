c665d81550821b4007ec93a4f2d4d5653d8bb06f
barakda <barak.davidov@gmail.com>
2024-01-31 17:53:08 +0200

    check push to quay
    Signed-off-by: barakda <barak.davidov@gmail.com>

--- mk/containerized.mk
+++ mk/containerized.mk
@@ -23,16 +23,9 @@ pull: ## Download SVC images
 build:  ## Build SVC images
 build: DOCKER_COMPOSE_ENV = DOCKER_BUILDKIT=1 COMPOSE_DOCKER_CLI_BUILD=1
 
-GIT_LATEST_TAG != git describe --tags --abbrev=0 2>/dev/null
 push: ## Push nvmeof and nvmeof-cli containers images to quay.io registries
-	docker tag $(QUAY_NVMEOF):$(VERSION) $(QUAY_NVMEOF):$(GIT_LATEST_TAG)
-	docker tag $(QUAY_NVMEOFCLI):$(VERSION) $(QUAY_NVMEOFCLI):$(GIT_LATEST_TAG)
-	docker tag $(QUAY_NVMEOF):$(VERSION) $(QUAY_NVMEOF):latest
-	docker tag $(QUAY_NVMEOFCLI):$(VERSION) $(QUAY_NVMEOFCLI):latest
-	docker push $(QUAY_NVMEOF):$(GIT_LATEST_TAG)
-	docker push $(QUAY_NVMEOFCLI):$(GIT_LATEST_TAG)
-	docker push $(QUAY_NVMEOF):latest
-	docker push $(QUAY_NVMEOFCLI):latest
+	docker push $(QUAY_NVMEOF):$(VERSION)
+	docker push $(QUAY_NVMEOFCLI):$(VERSION)
 
 run: ## Run command CMD inside SVC containers
 run: override OPTS += --rm
