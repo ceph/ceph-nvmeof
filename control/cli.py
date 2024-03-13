9904bd5fc118a55a1f0cf2d2f7d363950c1fda44
barakda <barak.davidov@gmail.com>
2024-03-13 14:37:53 +0200

    release 1.1.0 update
    Signed-off-by: barakda <barak.davidov@gmail.com>

--- control/cli.py
+++ control/cli.py
@@ -26,7 +26,7 @@ from .config import GatewayConfig
 from .utils import GatewayUtils
 from .utils import GatewayEnumUtils
 
-BASE_GATEWAY_VERSION="1.0.0"
+BASE_GATEWAY_VERSION="1.1.0"
 
 def errprint(msg):
     print(msg, file = sys.stderr)
