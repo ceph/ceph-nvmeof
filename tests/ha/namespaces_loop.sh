33fb6f65fc8c381455b0a4a37670db56ee472d1d
Alexander Indenbaum <aindenba@redhat.com>
2024-05-22 07:08:53 +0000

    ha test for namespaces
    replace unreliable pytests test
    
    Signed-off-by: Alexander Indenbaum <aindenba@redhat.com>

---
+++ tests/ha/namespaces_loop.sh
@@ -0,0 +1,12 @@
+# Check if GITHUB_WORKSPACE is defined
+if [ -n "$GITHUB_WORKSPACE" ]; then
+    test_dir="$GITHUB_WORKSPACE/tests/ha"
+else
+    test_dir=$(dirname $0)
+fi
+
+ITERATIONS=2
+for i in $(seq $ITERATIONS); do
+  echo "Iteration #$i"
+  source $test_dir/namespaces.sh
+done
