e08f42aaef5ffaa2686cd735d0498594db8e9316
barakda <barak.davidov@gmail.com>
2024-08-05 20:27:06 +0300

    atom_tests_add
    Signed-off-by: barakda <barak.davidov@gmail.com>

--- tests/atom/cpArtifactAndCleanup.sh
+++ tests/atom/cpArtifactAndCleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo cp -r /root/.ssh/atom_backup/artifact/multiIBMCloudServers_m2/* /tmp/artifact
+sudo cp -r /root/.ssh/atom_backup/artifact/multiIBMCloudServers_m2 /tmp/artifact
 sudo ls -lta /tmp/artifact
 sudo chmod -R +rx /tmp/artifact
 rm -rf /home/cephnvme/busyServer.txt
