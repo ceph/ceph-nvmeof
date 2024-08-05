7f7a998a303e7530018377e6baa9d057075a1345
barakda <barak.davidov@gmail.com>
2024-08-05 15:42:19 +0300

    atom_tests_add
    Signed-off-by: barakda <barak.davidov@gmail.com>

--- tests/atom/atomEnvInit.sh
+++ tests/atom/atomEnvInit.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 
-ATOM_REPO_OWNER=$1
-ATOM_REPO_TOKEN=$2
-ATOM_BRANCH=$3
+ATOM_BRANCH=$1
+ATOM_REPO_OWNER=$2
+ATOM_REPO_TOKEN=$3
+ATOM_SHA=$4
+ATOM=$5
 
 echo "_2_ATOM_BRANCH : $ATOM_BRANCH"
 echo "_2_ATOM_REPO_OWNER : $ATOM_REPO_OWNER"
 echo "_2_ATOM_REPO_TOKEN : $ATOM_REPO_TOKEN"
 
 TRIMMED_ATOM_REPO_OWNER="${ATOM_REPO_OWNER%?}"
-
-cleanup_docker_images() {
-    local HOST=$1
-    ssh -o StrictHostKeyChecking=no root@$HOST << EOF
-    sudo docker ps -q | xargs -r sudo docker stop
-    sudo docker ps -q | xargs -r sudo docker rm -f
-    sudo yes | sudo docker system prune -fa
-    sudo docker ps
-    sudo docker images
-EOF
-}
 
 # # In case of merge to devel
 # if [ $NVMEOF_REPO_OWNER = 'devel' ]; then
@@ -53,37 +44,3 @@ if [ $? -ne 0 ]; then
     echo "Error: Failed to clone the atom repository."
     exit 1
 fi
-
-# Switch to given SHA
-cd /home/cephnvme/actions-runner-ceph/ceph-nvmeof-atom
-git checkout $ATOM_SHA
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to checkout the specified SHA."
-    exit 1
-fi
-
-# Build atom images based on the cloned repo
-docker build -t nvmeof_atom:$ATOM_SHA /home/cephnvme/actions-runner-ceph/ceph-nvmeof-atom
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to build Docker image."
-    exit 1
-fi
-
-# Remove ceph cluster
-docker run -v /root/.ssh:/root/.ssh nvmeof_atom:$ATOM_SHA ansible-playbook -i custom_inventory.ini cephnvmeof_remove_cluster.yaml --extra-vars 'SELECTED_ENV=multiIBMCloudServers_m2'
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to run cephnvmeof_remove_cluster ansible-playbook."
-    exit 1
-fi
-
-# Cleanup remain images after ceph cluster removal
-HOSTS=("cephnvme-vm9" "cephnvme-vm7" "cephnvme-vm6" "cephnvme-vm1")
-for HOST in "${HOSTS[@]}"; do
-    echo "Cleaning up Docker images on $HOST"
-    cleanup_docker_images "$HOST"
-    if [ $? -ne 0 ]; then
-        echo "Error: Failed to clean up Docker images on $HOST."
-    fi
-done
-
-sudo podman ps -q | xargs -r sudo podman stop; sudo podman ps -q | xargs -r sudo podman rm -f; sudo yes | podman system prune -fa; podman ps; podman images
