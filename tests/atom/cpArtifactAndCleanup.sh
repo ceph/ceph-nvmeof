b790ac9a2d946ccd692c09498d78f43c9faba90c
barakda <barak.davidov@gmail.com>
2024-08-15 17:25:34 +0300

    ci: update_atom_job
    Signed-off-by: barakda <barak.davidov@gmail.com>

--- tests/atom/cpArtifactAndCleanup.sh
+++ tests/atom/cpArtifactAndCleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -rf /tmp/artifact/multiIBMCloudServers_m2
+sudo rm -rf /tmp/artifact/multiIBMCloudServers_m2
 sudo cp -r /root/.ssh/atom_backup/artifact/multiIBMCloudServers_m2 /tmp/artifact
 sudo ls -lta /tmp/artifact
 sudo chmod -R +rx /tmp/artifact
