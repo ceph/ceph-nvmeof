3f2e921d25aa9d60e0b35401fd878a77d84c630f
Tomer Haskalovitch <il033030@tomers-mbp.givatayim.il.ibm.com>
2024-10-09 11:22:50 +0300

    add blocklist test and also add verification in state transition test
    Signed-off-by: hezko <tomer.haska@gmail.com>

--- tests/ha/state_transitions.sh
+++ tests/ha/state_transitions.sh
@@ -65,6 +65,29 @@ verify_ana_groups() {
     fi
 }
 
+verify_blocklist() {
+  stopped_gw_name=$1
+  NODE_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $stopped_gw_name)
+  BLOCKLIST=$(docker compose exec -T ceph ceph osd blocklist ls)
+
+  echo "verifying there is at least 1 entry in the blocklist related to the stopped gateway"
+  if echo "$BLOCKLIST" | grep -q "$NODE_IP"; then
+    echo "ip $NODE_IP for the stopped gateway was found the blocklist."
+  else
+      echo "ip $NODE_IP for node the stopped gateway was not found in blocklist."
+      exit 1
+  fi
+
+  echo "verifying there are no entries in the blocklist which are not related to the stopped gateway"
+  if echo "$BLOCKLIST" | grep -qv "$NODE_IP"; then
+    echo "found at least 1 entry in blocklist which is not related to gateway in the stopped gateway. failing"
+    exit 1
+  else
+      echo "didn't find unexpected entries which are not relaetd to the stopped gateway."
+  fi
+  echo "blocklist verification successful"
+}
+
 #
 # MAIN
 #
@@ -94,6 +117,7 @@ GW1_FAILOVER_OPTIMIZED=$(expect_optimized $GW1_NAME 2)
 gw1_ana1=$(access_number_by_index "$GW1_FAILOVER_OPTIMIZED" 0)
 gw1_ana2=$(access_number_by_index "$GW1_FAILOVER_OPTIMIZED" 1)
 verify_ana_groups "$gw1_ana1" "$gw1_ana2"
+verify_blocklist "$GW2_NAME"
 
 #
 # Step 3 failback
