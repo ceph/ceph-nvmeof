41809ec0c89963bdb9e330aa1220994ce0e5a254
hezko <tomer.haska@gmail.com>
2024-10-10 11:36:20 +0300

    better logging

--- tests/ha/blocklist.sh
+++ tests/ha/blocklist.sh
@@ -1,60 +1,34 @@
 source .env
 
-
-docker ps
-echo 111
 GW1_NAME=$(docker ps --format '{{.ID}}\t{{.Names}}' | awk '$2 ~ /nvmeof/ && $2 ~ /1/ {print $1}')
 echo $GW1_NAME
 NODE_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $GW1_NAME)
 echo $NODE_IP
 
+echo "clearing blocklist"
 docker compose exec -T ceph ceph osd blocklist clear
 
+echo "shutting down gw1:$GW1_NAME"
 docker stop $GW1_NAME
 
+echo "waiting for 30s after shutdown"
 sleep 30
-docker ps
+
 BLOCKLIST=$(docker compose exec -T ceph ceph osd blocklist ls)
 echo $BLOCKLIST
-echo $BLOCKLIST | grep -q $NODE_IP
 
-echo 222
+echo "validating there is at least 1 entriy in the blocklist related to gw1"
+if echo "$BLOCKLIST" | grep -q "$NODE_IP"; then
+    echo "gw1 has been successfully stopped and its IP $NODE_IP is in the blocklist."
+else
+    echo "ip $NODE_IP for node gw1 was not found in blocklist."
+    exit 1
+fi
 
-
-
-
-# ceph nvme-gw show $RBD_POOL ''
-
-
-# # docker compose exec -T ceph ceph orch ps --refresh
-# NODE_IP=$(docker compose exec -T ceph ceph orch host ls --format json | jq -r ".[] | select(.hostname==\"$GW1_NAME\") | .public_addr")
-# echo $NODE_IP
-
-# # Check if NODE_IP was found
-# if [ -z "$NODE_IP" ]; then
-#     echo "Error: Could not find IP for node $NODE."
-#     exit 1
-# fi
-
-# # Stop the node
-# ceph orch daemon stop "$NODE"
-
-# # Wait for a moment to ensure the node has stopped
-# sleep 2
-
-# # Check the blocklist for the node's IP
-# BLOCKLIST=$(ceph osd blocklist ls)
-
-# if echo "$BLOCKLIST" | grep -q "$NODE_IP"; then
-#     echo "Node $NODE has been successfully stopped and its IP $NODE_IP is in the blocklist."
-# else
-#     echo "Error: IP $NODE_IP for node $NODE not found in blocklist."
-#     exit 1
-# fi
-
-
-# docker compose exec ceph ceph orch daemon stop {NODE}
-
-# docker compose exec ceph ceph osd blocklist ls
-
-# docker compose exec ceph ceph orch daemon start
+echo "validating there are no entries in the blocklist which are not related to gw1"
+if echo "$BLOCKLIST" | grep -qv "$NODE_IP"; then
+    echo "found at least 1 entry in blocklist which is not related to gw1 in the stopped gw1. failing"
+    exit 1
+else
+    echo "didn't find entries unrelaetd to gw1."
+fi
