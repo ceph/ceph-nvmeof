b0739db0f921d511a101c2a5c9748a2201f784e2
hezko <tomer.haska@gmail.com>
2024-10-10 09:22:29 +0300

    try again

--- tests/ha/blocklist.sh
+++ tests/ha/blocklist.sh
@@ -1,14 +1,34 @@
+source .env
+
+
+docker ps
 echo 111
 GW1_NAME=$(docker ps --format '{{.ID}}\t{{.Names}}' | awk '$2 ~ /nvmeof/ && $2 ~ /1/ {print $1}')
 echo $GW1_NAME
-echo 222
-docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $GW1_NAME
-
-docker compose exec -T ceph ceph orch set backend cephadm
-docker compose exec -T ceph ceph orch ps --refresh
-
-NODE_IP=$(docker compose exec -T ceph ceph orch host ls --format json | jq -r ".[] | select(.hostname==\"$GW1_NAME\") | .public_addr")
+NODE_IP = docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $GW1_NAME
 echo $NODE_IP
+
+BLOCKLIST = docker compose exec -T ceph ceph osd blocklist clear
+
+docker stop $GW1_NAME
+
+sleep 30
+docker ps
+BLOCKLIST = docker compose exec -T ceph ceph osd blocklist ls
+echo $BLOCKLIST
+$BLOCKLIST | grep -q $NODE_IP
+
+echo 222
+
+
+
+
+# ceph nvme-gw show $RBD_POOL ''
+
+
+# # docker compose exec -T ceph ceph orch ps --refresh
+# NODE_IP=$(docker compose exec -T ceph ceph orch host ls --format json | jq -r ".[] | select(.hostname==\"$GW1_NAME\") | .public_addr")
+# echo $NODE_IP
 
 # # Check if NODE_IP was found
 # if [ -z "$NODE_IP" ]; then
