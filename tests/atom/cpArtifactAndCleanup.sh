0faa7052e567cdda15cd171053c3ff37807cd979
Barak Davidov <barakda@il.ibm.com>
2025-01-22 12:28:11 +0200

    atom nightly and sha update
    Signed-off-by: Barak Davidov <barakda@il.ibm.com>

--- tests/atom/cpArtifactAndCleanup.sh
+++ tests/atom/cpArtifactAndCleanup.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
-sudo rm -rf /home/cephnvme/artifact/*
-sudo ls -lta /home/cephnvme/artifact
+NIGHTLY=$1
 
-sudo rm -rf /home/cephnvme/artifact.tar.gz
-sudo ls -lta /home/cephnvme/
+process_artifacts() {
+    local artifact_dir=$1
+    local backup_dir=$2
+    local tar_file=$3
+    local busy_file=$4
 
-sudo cp -r /root/.ssh/atom_backup/artifact/multiIBMCloudServers_m6 /home/cephnvme/artifact
-sudo ls -lta /home/cephnvme/artifact
+    sudo rm -rf "${artifact_dir:?}"/*
+    sudo ls -lta "$artifact_dir"
 
-sudo tar -czf /home/cephnvme/artifact.tar.gz -C /home/cephnvme/artifact .
-sudo ls -lta /home/cephnvme/artifact
-sudo ls -lta /home/cephnvme
-sudo chmod +rx /home/cephnvme/artifact.tar.gz
-sudo rm -rf /home/cephnvme/busyServer.txt
+    sudo rm -rf "$tar_file"
+    sudo ls -lta "$(dirname "$artifact_dir")"
+    sudo cp -r "$backup_dir" "$artifact_dir"
+    sudo ls -lta "$artifact_dir"
+
+    sudo tar -czf "$tar_file" -C "$artifact_dir" .
+    sudo ls -lta "$artifact_dir"
+    sudo ls -lta "$(dirname "$artifact_dir")"
+    sudo chmod +rx "$tar_file"
+    sudo rm -rf "$busy_file"
+}
+
+if [ "$NIGHTLY" != "nightly" ]; then
+    process_artifacts \
+        "/home/cephnvme/artifact_m6" \
+        "/root/.ssh/atom_backup/artifact/multiIBMCloudServers_m6" \
+        "/home/cephnvme/artifact_m6.tar.gz" \
+        "/home/cephnvme/busyServer.txt"
+else
+    process_artifacts \
+        "/home/cephnvme/artifact_m7" \
+        "/root/.ssh/atom_backup/artifact/multiIBMCloudServers_m7" \
+        "/home/cephnvme/artifact_m7.tar.gz" \
+        "/home/cephnvme/busyServerNightly.txt"
+fi
