4469511664d787d90a228a9e81a778c8239b7993
Barak Davidov <barakda@il.ibm.com>
2025-04-07 00:17:40 +0300

    atom old ver nightly add
    Signed-off-by: Barak Davidov <barakda@il.ibm.com>

---
+++ tests/atom/clusterBuildTestsRun_7.1.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+set -e
+
+
+VERSION=$1
+CEPH_BRANCH=$2
+if [ "$3" = "latest" ]; then
+    CEPH_SHA=$(curl -s https://shaman.ceph.com/api/repos/ceph/$CEPH_BRANCH/latest/centos/9/ | jq -r ".[] | select(.archs[] == \"$(uname -m)\" and .status == \"ready\") | .sha1")
+else
+    CEPH_SHA=$3
+fi
+ATOM_SHA=$4
+ACTION_URL=$5
+NIGHTLY=$6
+
+echo "IBM_CLOUD_KEY in script: $6"
+
+RUNNER_NIGHTLY_FOLDER='/home/cephnvme/actions-runner-ceph-m8'
+BUSY_NIGHTLY_FILE='/home/cephnvme/busyServerNightly.txt'
+
+check_cluster_busy() {
+    local busy_file=$1
+    local action_url=$2
+
+    while true; do
+        if [ -f "$busy_file" ]; then
+            echo "The server is busy with another GitHub Action job, please wait..."
+            sleep 90
+        else
+            echo "The server is available for use!"
+            echo "$action_url" > "$busy_file"
+            chmod +rx "$busy_file"
+            break
+        fi
+    done
+}
+
+hostname
+rm -rf $RUNNER_NIGHTLY_FOLDER/ceph-nvmeof-atom
+sudo rm -rf /root/.ssh/atom_backup/artifact/multiIBMCloudServers_m8/*
+sudo ls -lta /root/.ssh/atom_backup/artifact/multiIBMCloudServers_m8
+cd $RUNNER_NIGHTLY_FOLDER
+
+
+# Cloning atom repo
+git clone git@github.ibm.com:NVME-Over-Fiber/ceph-nvmeof-atom.git
+
+# Switch to given SHA
+cd ceph-nvmeof-atom
+git checkout $ATOM_SHA
+
+# Build atom images based on the cloned repo
+sudo docker build -t nvmeof_atom:$ATOM_SHA .
+
+set -x
+
+check_cluster_busy "$BUSY_NIGHTLY_FILE" "$ACTION_URL"
+sudo docker run \
+    -v /root/.ssh:/root/.ssh \
+    nvmeof_atom:"$ATOM_SHA" \
+    python3 atom.py \
+    --project=nvmeof \
+    --ceph-img=quay.ceph.io/ceph-ci/ceph:"$CEPH_SHA" \
+    --ceph-branch="$CEPH_BRANCH" \
+    --gw-img=quay.io/ceph/nvmeof:"$VERSION" \
+    --cli-img=quay.io/ceph/nvmeof-cli:"$VERSION" \
+    --initiators=1 \
+    --gw-group-num=1 \
+    --gw-num=4 \
+    --gw-to-stop-num=1 \
+    --gw-scale-down-num=1 \
+    --subsystem-num=2 \
+    --ns-num=230 \
+    --subsystem-max-ns-num=1024 \
+    --failover-num=4 \
+    --failover-num-after-upgrade=2 \
+    --rbd-size=200M \
+    --seed=0 \
+    --vhosts=4 \
+    --fio-devices-num=1 \
+    --lb-timeout=20 \
+    --config-dbg-mon=10 \
+    --config-dbg-ms=1 \
+    --nvmeof-daemon-stop \
+    --nvmeof-systemctl-stop \
+    --mon-leader-stop \
+    --mon-client-kill \
+    --nvmeof-daemon-remove \
+    --redeploy-gws \
+    --github-action-deployment \
+    --journalctl-to-console \
+    --dont-power-off-cloud-vms \
+    --skip-lb-group-change-test \
+    --skip-block-list-test \
+    --skip-multi-hosts-conn-test \
+    --skip-ns-rebalancing-test \
+    --ibm-cloud-key=nokey \
+    --github-nvmeof-token=nokey \
+    --env=m8
+DOCKER_EXIT_STATUS=$?
+
+set +x
+
+if [ $DOCKER_EXIT_STATUS -eq 0 ]; then
+    echo "Atom docker run succeeded"
+else
+    echo "Atom docker run failed!!!"
+    exit 1
+fi
